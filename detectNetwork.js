d082a717172a4794d04d409888c6115b251d9d9b
David Randall <oakesrandall@gmail.com>
2017-04-24 14:55:44 -0600

    V1 completed and ready for testing, added Maestro and Discover Card

--- detectNetwork.js
+++ detectNetwork.js
@@ -5,12 +5,12 @@ var detectNetwork = function(cardNumber) {
   var stringToNumber = function (string, numberOfDigits) {
     var requestedDigits = string.substring(0, numberOfDigits);
   	return parseInt(requestedDigits, 10);
-  }
+  };
   // If the string is empty, let them know they didn't enter a number
   if (cardNumber === "") {
   	return "You didn't enter a card number";
   }
-  // Diner;s Club if statement	
+  // Diner"s Club if statement	
   else if (stringToNumber(cardNumber, 2) === 38 || (stringToNumber(cardNumber, 2) === 39) && cardNumber.length === 14) {
     cardType = "Diner's Club";
   }
@@ -26,12 +26,18 @@ var detectNetwork = function(cardNumber) {
   else if (stringToNumber(cardNumber, 2) === 34 || (stringToNumber(cardNumber, 2) === 37) && cardNumber.length === 15) {
     cardType = "MasterCard";
   }  
+  //  Discover Card if statement
+  else if ((stringToNumber(cardNumber, 4) === 6011) || ((stringToNumber(cardNumber, 6) >= 622126) && (stringToNumber(cardNumber, 6) <= 622925)) || ((stringToNumber(cardNumber, 3) >= 644) && (stringToNumber(cardNumber, 6) <= 649)) || (stringToNumber(cardNumber, 2) === 65) && (cardNumber.length === 16 ||  cardNumber.length === 19)) {
+    cardType = "Discover Card";
+  }   
   //  China UnionPay if statement
   else if (stringToNumber(cardNumber, 2) === 62 && (cardNumber.length >= 16 && cardNumber.length <= 19)) {
     cardType = "China UnionPay";
   }  
-  //  Switch/Maestro if statement
-
+  //  Maestro if statement
+  else if ((stringToNumber(cardNumber, 2) === 50) || ((stringToNumber(cardNumber, 2) >= 56) && (stringToNumber(cardNumber, 2) <= 58)) || ((stringToNumber(cardNumber, 1) === 6)) && (cardNumber.length >= 12 &&  cardNumber.length <= 19)) {
+    cardType = "Maestro";
+  }   
   // Else statement if card number doesn't match any
   else { 
     cardType = "Your card doesn't match known cards";
@@ -39,6 +45,7 @@ var detectNetwork = function(cardNumber) {
   // Returns the variable assigned to the type of card detected
   return cardType;
 };
+
 
 
 
