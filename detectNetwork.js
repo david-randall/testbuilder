c0cc9bc08c3e0417e2fef771fe850f8fb41093d9
David Randall <oakesrandall@gmail.com>
2017-04-24 14:09:04 -0600

    Add helper function to convert string and identify digits needed, added MasterCard, cleaned up code

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,39 +1,42 @@
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-  // create variable to turn the given string into a number
+  // Variable to hold type of card
   var cardType = "";
-  var firstTwoNumbers = cardNumber.substring(0, 2);
-  	if (cardNumber === "") {
-  	  return "You didn't enter a card number";
-  	}
+  // Helper function created to return the number of digits required to determine card type, converted from a string into a number
+  var stringToNumber = function (string, numberOfDigits) {
+    var requestedDigits = string.substring(0, numberOfDigits);
+  	return parseInt(requestedDigits, 10);
+  }
+  // If the string is empty, let them know they didn't enter a number
+  if (cardNumber === "") {
+  	return "You didn't enter a card number";
+  }
   // Diner;s Club if statement	
-  	else if ((parseInt(firstTwoNumbers, 10) === 38 || parseInt(firstTwoNumbers, 10) === 39) && cardNumber.length === 14) {
-  	  cardType = "Diner's Club";
-  	}
-  	
+  else if (stringToNumber(cardNumber, 2) === 38 || (stringToNumber(cardNumber, 2) === 39) && cardNumber.length === 14) {
+    cardType = "Diner's Club";
+  }
   //  American Express if statement
-    else if ((parseInt(firstTwoNumbers, 10) === 34 || parseInt(firstTwoNumbers, 10) === 37) && cardNumber.length === 15) {
-      cardType = "American Express";
-    }  
+  else if (stringToNumber(cardNumber, 2) === 34 || (stringToNumber(cardNumber, 2) === 37) && cardNumber.length === 15) {
+    cardType = "American Express";
+  }  
   //  Visa if statement
-
+  else if (stringToNumber(cardNumber, 1) === 4 && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
+    cardType = "Visa";
+  }   
   //  MasterCard if statement
-
+  else if (stringToNumber(cardNumber, 2) === 34 || (stringToNumber(cardNumber, 2) === 37) && cardNumber.length === 15) {
+    cardType = "MasterCard";
+  }  
   //  China UnionPay if statement
 
   //  Switch/Maestro if statement
 
   // Else statement if card number doesn't match any
-
-    } else { 
-  	  cardType = "Your card doesn't match known cards";
-  	}
+  else { 
+    cardType = "Your card doesn't match known cards";
+  }
+  // Returns the variable assigned to the type of card detected
+  return cardType;
 };
-
 
 
 
